ace8f197fb0316cc1b967ac91fd84055239309c4
TebibuS <tebibueren7@gmail.com>
2023-06-18 19:50:36 -0500

    Tebibu's TODO Week 2

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -1,60 +1,77 @@
 import React, { Component } from "react";
-import { Button, TextField } from "@mui/material";
+import { TextField, Button } from "@mui/material";
 
 class AddTodo extends Component {
-  // Create a local react state of the this component with a content property set to nothing.
   constructor() {
     super();
     this.state = {
       content: "",
+      preConditions: "",
+      acceptanceCriteria: "",
     };
   }
-  // The handleChange function updates the react state with the new input value provided from the user.
-  // "event" is the defined action a user takes. In this case, the event is triggered when the user types something
-  // into the text field.
+
   handleChange = (event) => {
     this.setState({
-      content: event.target.value,
+      [event.target.name]: event.target.value,
     });
   };
-  // The handleSubmit function collects the forms input and puts it into the react state.
-  // event.preventDefault() is called to prevents default event behavior like refreshing the browser.
-  // this.props.addTodo(this.state) passes the current state (or user input) into the addTodo function defined
-  // in the Home.js file which then adds the input into the list.
+
   handleSubmit = (event) => {
     event.preventDefault();
     if (this.state.content.trim()) {
-      this.props.addTodo(this.state);
+      this.props.addTodo({
+        content: this.state.content,
+        date: new Date().toLocaleString("en-US"),
+        preConditions: this.state.preConditions,
+        acceptanceCriteria: this.state.acceptanceCriteria,
+      });
       this.setState({
         content: "",
+        preConditions: "",
+        acceptanceCriteria: "",
       });
     }
   };
+
   render() {
     return (
-      // 1. When rendering a component, you can render as many elements as you like as long as it is wrapped inside
-      // one div element.
-      // 2. The return statement should include a text field input with the handleChange function from above that
-      // is passed into an onChange event.
-      // 3. The return should also include a button with the handleSubmit function from above that is passed into
-      // an OnClick event.
-      // 4. The value of the text field also should reflect the local state of this component.
-      <div>
+      <form onSubmit={this.handleSubmit}>
         <TextField
-          label="Add New Item"
+          label="Task"
           variant="outlined"
+          name="content"
           onChange={this.handleChange}
           value={this.state.content}
+          fullWidth
+          margin="normal"
         />
-        <Button
-          style={{ marginLeft: "10px" }}
-          onClick={this.handleSubmit}
-          variant="contained"
-          color="primary"
-        >
-          Add
+        <TextField
+          label="Pre-Conditions"
+          variant="outlined"
+          name="preConditions"
+          onChange={this.handleChange}
+          value={this.state.preConditions}
+          fullWidth
+          multiline
+          rows={2}
+          margin="normal"
+        />
+        <TextField
+          label="Acceptance Criteria"
+          variant="outlined"
+          name="acceptanceCriteria"
+          onChange={this.handleChange}
+          value={this.state.acceptanceCriteria}
+          fullWidth
+          multiline
+          rows={4}
+          margin="normal"
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Add Task
         </Button>
-      </div>
+      </form>
     );
   }
 }
